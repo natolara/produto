9af4213dc6517f506a509f4d2f10169aa23be5e3
Renato Lara <larasantos22@hotmail.com>
2021-04-27 22:19:38 -0300

    Projeto criado

---
+++ src/main/java/com/site/escola/SiteEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.site.escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteEscolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/site/escola/SiteEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
