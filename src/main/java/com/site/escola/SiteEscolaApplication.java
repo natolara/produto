9fc148343c9191e4f2abc3b9d9fc1b36d21a309a
Renato Lara <larasantos22@hotmail.com>
2021-05-04 14:42:07 -0300

    Criando operacao de intanciacao

--- src/main/java/com/site/escola/SiteEscolaApplication.java
+++ src/main/java/com/site/escola/SiteEscolaApplication.java
@@ -1,13 +1,32 @@
 package com.site.escola;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SiteEscolaApplication {
+import com.site.escola.domain.Categoria;
+import com.site.escola.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SiteEscolaApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SiteEscolaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorios");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
